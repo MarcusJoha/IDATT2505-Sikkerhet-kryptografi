6e08626d5309cbcd787ae9409a40e6beb64187b3
Marcus Johannessen <marcusjo@stud.ntnu.no>
2022-11-13 12:28:32 +0100

    oppg5 nu

--- oblig9/oppg4.py
+++ oblig9/oppg4.py
@@ -1,5 +1,66 @@
-
-n = 432424
-
+from sympy import sieve
 
 
+class RSA:
+    
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+        
+    def gcd(self, a, b) -> int:
+        r = a%b
+        while(r):
+            a = b
+            b = r
+            r = a%b
+        return b
+            
+    def ext_euclid(self, a, b):
+        if a == 0 :
+            return b,0,1
+        gcd,x1,y1 = self.ext_euclid(b%a, a)
+        x = y1 - (b//a) * x1
+        y = x1
+        return gcd,x,y
+            
+            
+    def rsa_cryptate(self, m, p, q):
+        return (m**p)%q
+
+    def rsa_decryptate(self, e, p, q):
+        return (e**p)%q
+
+
+    def rsa_keygen(self):
+        n = self.p*self.q
+        phi = (self.p-1)*(self.q-1)
+        e = 3
+        while(self.gcd(e, phi) != 1):
+            e += 1
+        gcd, d, y = self.ext_euclid(e, phi)
+        return e, n, d
+
+       
+        
+if __name__ == '__main__':
+    
+    # list of primes in range smallest 16 bit number to largest 16 bit number
+    primes = sieve.primerange(int(0b1000000000000000), int(0b1111111111111111))
+    primes_list = list(primes)
+    # print(primes_list)
+    
+    rsa = RSA(primes_list[0], primes_list[1])
+    
+    # Prøvde å finn riktige verdier men funket ikke helt
+    # e,n,d = rsa.rsa_keygen()
+    # print("e: {},n: {} ,d: {}".format(e, n, d)) 
+    
+    
+    print((923827*1388389)%3)
+    
+    # Fikk litt hjelp med å finne riktige nøkkelparr 
+    encrypted = rsa.rsa_cryptate(12345, 3, 1388389)
+    print("Meldingen '12345' kryptert: ", encrypted)
+    decrypted = rsa.rsa_decryptate(encrypted,923827, 1388389)
+    print("Meldingen '{}' dekryptert: {}".format(encrypted, decrypted))
+    
--- oblig9/test.py
+++ oblig9/test.py
@@ -1,35 +1,38 @@
-def find_period_a(key):
-    period = 0
-    z = 0b0
-    bitstring = "{:04b}".format(key)
-    while(key != z):
-        bit = (int(bitstring[0]) + int(bitstring[1]) + int(bitstring[2]) + int(bitstring[3])) % 2
-        bitstring = bitstring + str(bit)
-        z = int(bitstring, 2)&0b1111
-        print("{:04b}".format(z))
-        period += 1
-        bitstring = "{:04b}".format(z)
-    print("Antall perioder: " + str(period))
+import sympy as sy
 
-def find_period_b(key):
-    period = 0
-    z = 0b0
-    bitstring = "{:04b}".format(key)
-    while(key != z):
-        bit = (int(bitstring[0]) + int(bitstring[3])) % 2
-        bitstring = bitstring + str(bit)
-        z = int(bitstring, 2)&0b1111
-        print("{:04b}".format(z))
-        period += 1
-        bitstring = "{:04b}".format(z)
-    print("Antall perioder: " + str(period))
+def euclid(a, b):
+    r = a%b
+    while(r):
+        a = b
+        b = r
+        r = a%b
+    print(" GDC: " + str(b))
+
+def ext_euclid(a, b):
+    # Base Case
+    if a == 0 :
+        return b,0,1
+             
+    gcd,x1,y1 = ext_euclid(b%a, a)
+     
+    # Update x and y using results of recursive
+    # call
+    x = y1 - (b//a) * x1
+    y = x1
+     
+    return gcd,x,y
+
+def crypt_rsa(m, k):
+    return (m**k[0])%k[1]
+
+def decrypt_rsa(e, k):
+    return (e**k[0])%k[1]
 
 if __name__ == "__main__":
-    print("Oppgave 3 a)\nFinn perioden for K=> '1000' '0011' '1111'")
-    find_period_a(0b1000)
-    find_period_a(0b0011)
-    find_period_a(0b1111)
-    print("\n\nb) \nFinn perioden for samme nøkler med annen LFSR")
-    find_period_b(0b1000)
-    find_period_b(0b0011)
-    find_period_b(0b1111)
+    #relativt prim betyr at gdc = 1
+    #print(list(sy.sieve.primerange(500, 2000)))
+    encrypted = crypt_rsa(12345, [3, 1388389])
+    print("Meldingen '12345' kryptert: ", encrypted)
+    decrypted = decrypt_rsa(encrypted,[923827, 1388389])
+    print("Meldingen '{}' dekryptert: {}".format(encrypted, decrypted))
+    
